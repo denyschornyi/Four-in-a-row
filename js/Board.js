70ac08953056eecf44616e3868eb679355405eb9
Denys <chornijj14@gmail.com>
2020-06-10 19:41:41 +0200

    Fill fully Board class to create an empty spaces

--- js/Board.js
+++ js/Board.js
@@ -1,8 +1,24 @@
 class Board {
     constructor() {
-        this.row = 6;
+        this.row = 7;
         this.columns = 6;
-        this.spaces = [];
+        this.spaces = this.createSpaces();
+    }
+    
+    createSpaces(){
+        const spaces = [];
+
+        for(let x = 0; x < this.columns; x++){
+            const column = [];
+
+            for(let y = 0; y < this.row; y++){
+                let space = new Space(x, y);
+                column.push(space);
+            }
+
+            spaces.push(column);
+        }
+        return spaces;
     }
 }
 
