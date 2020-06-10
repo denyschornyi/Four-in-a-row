cc037f2a8805e3c5406ce837ab3adfcbf375d2e7
Denys <chornijj14@gmail.com>
2020-06-10 11:45:11 +0200

    Created a createtoken method

--- js/Player.js
+++ js/Player.js
@@ -4,9 +4,18 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.token = [];
+        this.token = this.createTokens(21);
+    }
+
+    createTokens(num){
+        const tokens = [];
+        for(let i = 0; i < num; i++){
+            let token = new Token(i, this);
+            tokens.push(token);
+        }
+        return tokens;
     }
 }
 
 // let Denys = new Player('Denys', '12321', 'blue');
-// console.log(Denys);
+// console.log(Denys.token);
