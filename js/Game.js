b862f457aea9b07e4825335ad89302f8f9e5d7cd
Denys <chornijj14@gmail.com>
2020-06-15 16:03:30 +0200

    Created a Game class and wrote a consctructor mathod

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,15 @@
 class Game {
-    
+    constructor(){
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }    
+
+
+    createPlayers(){
+        let players = [     ['Player 1', '#e15258', 1, true],
+                            ['Player 2', '#e59a13', 2, false]];
+        return players;
+
+    }
 }
