06e608bda6600aa0e70755f62fe56b631f354d8a
Denys <chornijj14@gmail.com>
2020-06-18 11:38:39 +0200

    Check player

--- js/Game.js
+++ js/Game.js
@@ -52,4 +52,59 @@ class Game {
     		activeToken.drop(targetSpace);   
         }              
     }
+
+    checkForWin(target){
+        const owner = target.token.owner;
+        let win = false;
+    
+        // vertical
+        for (let x = 0; x < this.board.columns; x++ ){
+            for (let y = 0; y < this.board.rows - 3; y++){
+                if (this.board.spaces[x][y].owner === owner && 
+                    this.board.spaces[x][y+1].owner === owner && 
+                    this.board.spaces[x][y+2].owner === owner && 
+                    this.board.spaces[x][y+3].owner === owner) {
+                        win = true;
+                }           
+            }
+        }
+    
+        // horizontal
+        for (let x = 0; x < this.board.columns - 3; x++ ){
+            for (let y = 0; y < this.board.rows; y++){
+                if (this.board.spaces[x][y].owner === owner && 
+                    this.board.spaces[x+1][y].owner === owner && 
+                    this.board.spaces[x+2][y].owner === owner && 
+                    this.board.spaces[x+3][y].owner === owner) {
+                        win = true;
+                }           
+            }
+        }
+    
+        // diagonal
+        for (let x = 3; x < this.board.columns; x++ ){
+            for (let y = 0; y < this.board.rows - 3; y++){
+                if (this.board.spaces[x][y].owner === owner && 
+                    this.board.spaces[x-1][y+1].owner === owner && 
+                    this.board.spaces[x-2][y+2].owner === owner && 
+                    this.board.spaces[x-3][y+3].owner === owner) {
+                        win = true;
+                }           
+            }
+        }
+    
+        // diagonal
+        for (let x = 3; x < this.board.columns; x++ ){
+            for (let y = 3; y < this.board.rows; y++){
+                if (this.board.spaces[x][y].owner === owner && 
+                    this.board.spaces[x-1][y-1].owner === owner && 
+                    this.board.spaces[x-2][y-2].owner === owner && 
+                    this.board.spaces[x-3][y-3].owner === owner) {
+                        win = true;
+                }           
+            }
+        }
+    
+        return win;
+    }
 }
