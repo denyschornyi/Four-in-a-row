f7fa8fd367927860449acd787847eab25e6cd5f4
Denys <chornijj14@gmail.com>
2020-06-18 11:15:35 +0200

    Action to drop token

--- js/Game.js
+++ js/Game.js
@@ -30,8 +30,27 @@ class Game {
                 this.activePlayer.activeToken.moveRight(this.board.columns);
             }
             if(event.key === 'ArrowDown'){
-                
+                this.activePlayer.activeToken.drop(this.board);   
+                this.playToken();
             }
         } 
+    }
+
+    playToken(){
+        let spaces = this.board.spaces;
+        let activeToken = this.activePlayer.activeToken;
+        let taregtColumn = spaces[activeToken.columnLocation];
+        let targetSpace = null;
+
+        for(let space of taregtColumn){
+            if(space.token === null){
+                targetSpace = space;
+            }
+        }
+
+        if(targetSpace !== null){
+            game.ready = false;
+            activeToken.drop(targetSpace);
+        }
     }
 }
--- js/Token.js
+++ js/Token.js
@@ -37,7 +37,7 @@ class Token {
     drop(target, reset){
         this.dropped = true;
         $(this.htmlToken).animate({
-            top: (target.y * target*diameter)
+            top: (target.y * target.diameter)
         }, 750, 'easeOutBounce', reset);
     }
     
